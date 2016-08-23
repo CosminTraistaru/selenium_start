275acf473a0a8e72291409e3b2df5b351e3599e2
Cosmin Traistaru <cosmin.traistaru@3pillarglobal.com>
2016-08-23 21:31:54 +0300

    changed test name

--- selenium_start/tests/test_site.py
+++ selenium_start/tests/test_site.py
@@ -3,14 +3,14 @@ import pytest
 from selenium_start.pages.landing import LandingPage
 
 
-def test_emag_search(selenium, variables):
+def test_amazon_search(selenium, variables):
     landing_page = LandingPage(selenium, variables, open_url=True)
     search_results = landing_page.search('nexus')
     assert selenium.title == 'Amazon.com: nexus'
 
 
 @pytest.mark.xfail
-def test_emag_search_fail(selenium, variables):
+def test_amazon_search_fail(selenium, variables):
     landing_page = LandingPage(selenium, variables, open_url=True)
     search_results = landing_page.search('aer conditionat')
     search_results.click_second_product()
